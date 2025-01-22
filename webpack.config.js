efd9c59aa03c02130c6077bf4511a2866b28bf52
asheux <brian.mboya@protonmail.com>
2025-01-22 14:43:07 +0300

    Fix build

--- webpack.config.js
+++ webpack.config.js
@@ -1,71 +1,72 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    entry: {
-        index: path.join(__dirname, "./", "index.js"),
-    },
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "[name].[contenthash].js",
-        publicPath: "/",
-    },
-    mode: "development",
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: "Asheux",
-            template: path.join(__dirname, "./", "index.html"),
-        }),
-    ],
-    devServer: {
-        port: 3000,
-        historyApiFallback: { index: "/", disableBotRule: true },
-        hot: true,
-    },
-    optimization: {
-        moduleIds: "deterministic",
-        runtimeChunk: "single",
-        splitChunks: {
-            cacheGroups: {
-                vendor: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: "vendors",
-                    chunks: "all",
-                },
-            },
+  entry: {
+    index: path.join(__dirname, "./", "index.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contenthash].js",
+    publicPath: "/",
+  },
+  mode: "development",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Asheux",
+      template: path.join(__dirname, "./", "index.html"),
+    }),
+  ],
+  devServer: {
+    port: 3000,
+    historyApiFallback: { index: "/", disableBotRule: true },
+    hot: true,
+  },
+  optimization: {
+    moduleIds: "deterministic",
+    runtimeChunk: "single",
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all",
         },
+      },
     },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: [
-                    "style-loader",
-                    "css-loader"
-                ],
-            },
-            {
-                test: /\.html$/i,
-                loader: "html-loader"
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif|pdf|txt)$/i,
-                type: "asset/resource",
-            },
-            {
-                test: /\.js$/,
-                include: path.resolve(__dirname, "./"),
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ["@babel/preset-env"],
-                    },
-                },
-            },
-        ],
-    },
-    experiments: {
-        asyncWebAssembly: true
-    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|pdf|txt)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, "./"),
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.wasm$/i,
+        type: "webassembly/async",
+      },
+    ],
+  },
+  experiments: {
+    asyncWebAssembly: true,
+  },
 };
